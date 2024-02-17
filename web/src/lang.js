b36b255f304b14f432038cc1b34e0549c05b20db
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-02-17 13:40:13 +0100

    added more languages

--- web/src/lang.js
+++ web/src/lang.js
@@ -5,16 +5,43 @@
  ******************************************************************************/
 
 export let courseInfo1 = {
-  de: "Diese Seite wird in Ihrem Browser ausgeführt und speichert keine Daten auf Servern.",
   en: "This page runs in your browser and does not store any data on servers.",
+  de: "Diese Seite wird in Ihrem Browser ausgeführt und speichert keine Daten auf Servern.",
+  es: "Esta página se ejecuta en su navegador y no almacena ningún dato en los servidores.",
+  it: "Questa pagina viene eseguita nel browser e non memorizza alcun dato sui server.",
+  fr: "Cette page fonctionne dans votre navigateur et ne stocke aucune donnée sur des serveurs.",
 };
 
 export let courseInfo2 = {
-  de: "Sie können diese Seite *, um neue randomisierte Aufgaben zu erhalten.",
   en: "You can * this page in order to get new randomized tasks.",
+  de: "Sie können diese Seite *, um neue randomisierte Aufgaben zu erhalten.",
+  es: "Puedes * esta página para obtener nuevas tareas aleatorias.",
+  it: "È possibile * questa pagina per ottenere nuovi compiti randomizzati",
+  fr: "Vous pouvez * cette page pour obtenir de nouvelles tâches aléatoires",
 };
 
 export let courseInfo3 = {
-  de: "aktualisieren",
   en: "reload",
+  de: "aktualisieren",
+  es: "recargar",
+  it: "ricaricare",
+  fr: "recharger",
+};
+
+// TODO: not yet used
+export let feedbackOK = {
+  en: ["awesome", "great", "correct", "well done"],
+  de: ["super", "gut gemacht", "weiter so", "richtig"],
+  es: ["impresionante", "genial", "correcto", "bien hecho"],
+  it: ["fantastico", "grande", "corretto", "ben fatto"],
+  fr: ["génial", "super", "correct", "bien fait"],
+};
+
+// TODO: not yet used
+export let feedbackErr = {
+  en: ["try again", "still some mistakes", "wrong answer"],
+  de: ["leider falsch", "nicht richtig", "versuch's nochmal"],
+  es: ["inténtalo de nuevo", "todavía algunos errores", "respuesta incorrecta"],
+  it: ["riprova", "ancora qualche errore", "risposta sbagliata"],
+  fr: ["réessayer", "encore des erreurs", "mauvaise réponse"],
 };
