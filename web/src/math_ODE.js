29bae6b0db1a94b77f4449f07a93a8b4a43cc18b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2024-03-08 16:48:06 +0100

    Update math_ODE.js

--- web/src/math_ODE.js
+++ web/src/math_ODE.js
@@ -18,67 +18,73 @@ import { Term, TermNode, heapsAlgorithm, range } from "./math.js";
  *     sqrt(C-12*x^3)/3         [output from sympy]
  *
  * In the example, the constant C of one of the terms must be multiplied by
- * a factor to get two equivalent terms. If the C of the first term is replaced
- * by K*C, and we determine K=1/6, then term comparison can be applied!
+ * a factor to get two equivalent terms. If C of the first term is replaced
+ * by K*C, with K=1/6, then term comparison can be applied!
  *
  * Furthermore, the names of constant variables may be swapped by the student.
  * Both  C1 * exp(2x) + C2 * exp(-4x)  and  C2 * exp(2x) + C1 * exp(-4x)
- * are equivalent. Our comparison algorithm has check for all permutations.
- * If all permutations fail, then the terms are not equal.
+ * are valid solutions. Our comparison algorithm has to check all permutations.
+ * If all permutations fail, then terms are considered not to be equal.
  *
- * ALGORITHM to compare two ODE-terms  u  and  v:
+ * ALGORITHM to compare two ODE solution-terms  u  and  v:
  *
  *   1. Try to compare both terms numerically, as implemented in method
- *      Term.compare(..). If the test succeeds, then things are easy.
+ *      Term.compare(..) in file main.js. If the test succeeds, then QUIT WITH
+ *      SUCCESS.
  *
- *   2. Reduce subterms that only contain C_i and constants, by C_i.
- *      For example we rewrite  sin(exp(cos(C+3))) + 3*C1 + sin(C1+C2)
- *      to  C + C1 + sin(C1+C2).  This is done, by recursively (depth first)
- *      checking, if an k-ary operation has one occurrence of C_j and all other
- *      (k-1) operands are constants. If this is applicable, replace the
- *      operation by C_i.
+ *   2. Reduce those subterms that only contain C_i and constants as operands
+ *      to just by C_i.
+ *      For example we rewrite
+ *           sin(exp(cos(C+3))) + 3*C1 + sin(C1+C2)
+ *      to
+ *           C + C1 + sin(C1+C2).
+ *      This is done, by checking recursively and depth-first, if an operation
+ *      only deals with C_j (with fixed j) and constant operations.
+ *      If this is applicable, then replace the operation by C_i.
  *
- *   3. Rename constants to C0, C1, ... . For example,  C * exp(-3*x)  is
- *      rewritten to  C0 * exp(-3*x).
+ *   3. Rename constants to C0, C1, ... .
+ *      For example,  C * exp(-3*x)  is rewritten to  C0 * exp(-3*x).
  *
- *   4. Generate all permutations of the list [0,1,...,n], with n, the order
- *      of the ODE.
+ *   4. Generate all permutations of the list [0,1,...,n-1],
+ *      with n, the order of the ODE.
  *
  *   5. For all permutations from the last step:
  *
- *   5.a)  In ONE of the terms to compare, rename constant variables,
- *         i.e. Ci becomes C{permutation(i)}
+ *   5.a)  Rename constant variables either in  u  (or in v, alternatively),
+ *         sucht that  Ci  becomes  C{permutation(i)}.
  *
- *         ATTENTION: The next sub steps work must work on COPIES of u and v!
+ *         ATTENTION: The next subordinate steps must be applied on COPIES of
+ *         terms u and v!
  *
- *   5.b)  For each 0 <= i < n, set Cj with i != j in both terms u and v to
- *         zero, so we are focussing on just one Ci.
+ *   5.b)  For each 0 <= i < n, set Cj with i != j to zero (in both terms),
+ *         so we are focussing on just one Ci over each iteration in 5.b)
  *
- *   5.b.1)  In ONE of the terms, replace Ci by (K*Ci). We will find a suitable
- *           K \in \R in the following.
+ *   5.b.1)  Replace Ci by (K*Ci) in either  u  or  v  (NOT in both).
+ *           We will try to determine K \in \R in the following steps.
  *
  *   5.b.2)  To get K, set all occurring variables, except K, to random values
  *           \in \C (bounded to range [-1, 1] on both axes, to circumvent
  *           numerical instability).
  *
  *   5.b.3)  Minimize f(K) = abs(u - v), where K is the only decision variable.
- *           The problem should be convex in the context of ODE-class-exercises.
+ *           The problem should be convex in the context of ODE-school-exercises.
  *           Sophisticated algorithms are available for this non-linear
  *           optimization tasks, but our math engine in file math.js is weak...
  *           So, we naively initialize K=0, as well as a step width S that is
- *           initialized to e.g. 1. In a (finite) sequence of iterations, we
- *           check, chose min { f(K-S), f(K), f(+S) }. In case that the
- *           "direction" changes, S is replaced by S/2.
+ *           initialized to e.g. 1.
+ *           In a (finite) sequence of iterations, we chose
+ *               min { f(K-S), f(K), f(K+S) }.
+ *           In case that the "direction" changes, S is replaced by S/2.
  *
- *   5.b.4)  Now check (with 10+ numerical tests), if our K from the last step
- *           let to equivalency. If not, continue with the next permutation in
- *           step 5.
+ *   5.b.4)  Now check (with 10+ numerical tests), if our concrete  K from the
+ *           last step yields in equivalency. If not, continue with the next
+ *           permutation in step 5.
  *
  *   5.c)  If we get here, all K's for the present permutation are known.
- *         We now numerically check, if terms u and v are equal under the
- *         factors K. If we succeed, then QUIT WITH SUCCESS.
+ *         We now numerically check, if terms  u  and  v  are equivalent
+ *         under the factors K. If we succeed, then QUIT WITH SUCCESS.
  *
- *   6. In case no of the permutations showed to be correct, then QUIT WITH
+ *   6. In case that none of the permutations tests succeeded, then QUIT WITH
  *      FAILURE.
  */
 
@@ -91,14 +97,15 @@ import { Term, TermNode, heapsAlgorithm, range } from "./math.js";
 export function compareODE(tu, tv) {
   let EPS = 1e-9;
 
-  // first try comparing conventionally... maybe we are in luck:
+  // first try comparing terms as implemented in file math.js
+  // ... maybe we are in luck!
   if (Term.compare(tu, tv)) return true;
 
   // Some of the following steps are destructive, so work on copies!
   tu = tu.clone();
   tv = tv.clone();
 
-  // Reduce subterms that only contain C_i by C_i (e.g. "e^C" -> "C").
+  // Reduce subterms that only contain C_i as variable (e.g. "e^C" -> "C").
   prepareODEconstantComparison(tu.root);
   prepareODEconstantComparison(tv.root);
 
@@ -150,7 +157,7 @@ export function compareODE(tu, tv) {
     let success = true;
     for (let k = 0; k < N; k++) {
       // For the fixed C_k, replace "C_k" by "(K * C_k)" in tv and check,
-      // if there is a K in RR which results in comparing terms
+      // if there is a K \in \R which results in comparing terms
       // successfully.
       let ckId = "C" + k;
       /** @type {Object.<string,TermNode>} */
@@ -195,11 +202,10 @@ export function compareODE(tu, tv) {
 
 /**
  * Naive implementation for convex optimization with one decision variable.
- * WARNING: Due to the primitivity of the math engine implemented in this
- *          file, we do not have the opportunity for automatic differentiation
- *          etc. This method should only be used for special cases.
- *          Currently, it is used to compare ODE solutions numerically.
- *          Refer to
+ * WARNING: Due to the primitivity of the math engine implemented in file
+ *          math.js, we do not have the opportunity for automatic
+ *          differentiation etc. This method should only be used for special
+ *          cases.
  * @param {Term} term -- the term
  * @param {string} dvId -- name of the decision variable; all other
  *                         variables MUST be set by constants in the dict
